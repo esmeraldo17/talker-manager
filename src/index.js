291c33d25043c9f77045181655008554ed1da5d5
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-01-23 16:53:11 +0100

    feat: criando endpoint /talker/:id ou seja procura por um talker especifico

--- src/index.js
+++ src/index.js
@@ -20,3 +20,15 @@ app.get('/talker', async (_req, res) => {
   const data = await readFile();
   res.status(HTTP_OK_STATUS).json(data);
 });
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = await readFile();
+  const findData = data.find((talker) => talker.id === Number(id));
+
+  if (!findData) {
+    res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+
+  res.status(HTTP_OK_STATUS).json(findData);
+});
